a09b5d9406ce43c207fb41a48e2b7372ec8ace79
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-12-08 22:24:52 -0500

    Better Spikes Clean Start

---
+++ src/main/java/lemon_juice/better_spikes/BetterSpikes.java
@@ -0,0 +1,32 @@
+package lemon_juice.better_spikes;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(BetterSpikes.MOD_ID)
+public class BetterSpikes {
+    public static final String MOD_ID = "better_spikes";
+
+    public BetterSpikes(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){}
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
